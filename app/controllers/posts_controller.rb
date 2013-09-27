39e8bc4d74c6a1c73c717a8bd0a34e27f855bbeb
tomgrim1 <tgrimley1@gmail.com>
2013-09-27 12:17:56 +0100

    Added posts and an index page.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
+
+	def show
+		
+	end
+
+	def create
+
+	end
+
+	def new
+
+	end
+
+	def update
+
+	end
+
+	def edit
+
+	end
+
+	def destroy
+
+	end
+end
